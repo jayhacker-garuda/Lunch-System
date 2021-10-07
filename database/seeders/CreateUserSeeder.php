4f1d2c6c66951baf8756db904b149eede9e6fabf
Sham Don | Top Coder <77687166+jayhacker-garuda@users.noreply.github.com>
2021-10-06 23:24:48 -0500

    Second Commit To The Lunch-System Seeder added

---
+++ database/seeders/CreateUserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class CreateUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'user_type' => 'sham-don',
+            'password' => bcrypt('12345678')
+        ]);
+    }
+}
